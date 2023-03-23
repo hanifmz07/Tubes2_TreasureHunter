8258afa8ab9649770b4f2ebd078282fb808a866a
fazelginanda <fzginanda@gmail.com>
2023-03-23 13:58:33 +0700

    Fix DFS

--- Tubes2_TreasureHunter/DFS.cs
+++ Tubes2_TreasureHunter/DFS.cs
@@ -19,11 +19,11 @@ namespace Tubes2_TreasureHunter
 
         public override void Solve(bool TSP)
         {
-            Peta a = peta;
+            Peta a = new Peta();
             bool repeat = TSP;
             a.Copy(peta);
             stack = new Stack<(Cell, Route)>();
-            stack.Push((a.Start, solusi));
+            stack.Push((a.Start, new Route()));
             int count = peta.NumTreasure;
 
             while (count > 0 && stack.Count > 0)
@@ -31,63 +31,75 @@ namespace Tubes2_TreasureHunter
                 (Cell, Route) accessing = stack.Pop();
                 Route temp = new Route(accessing.Item2);
                 temp.AddCell(accessing.Item1);
-                if (!(stack.Peek().Item1.Accessed))
+                if (accessing.Item1.Type == 3)
                 {
-                    if (accessing.Item1.Type == 3)
-                    {
-                        count--;
-                        a[accessing.Item1.X, accessing.Item1.Y].Type = 0;
-                        a[a.Start.X, a.Start.Y].Type = 2;
-                        a.Start = a[accessing.Item1.X, accessing.Item1.Y];
+                    count--;
+                    a[accessing.Item1.X, accessing.Item1.Y].Type = 0;
+                    a[a.Start.X, a.Start.Y].Type = 2;
+                    a.Start = a[accessing.Item1.X, accessing.Item1.Y];
 
-                        a.Inaccess();
-                        if (count <= 0)
+                    a.Inaccess();
+                    if (count <= 0)
+                    {
+                        if (repeat)
                         {
-                            if (repeat)
-                            {
-                                count++;
-                                a[peta.Start.X, peta.Start.Y].Type = 3;
-                                a[accessing.Item1.X, accessing.Item1.Y].Type = 0;
-                                a[a.Start.X, a.Start.Y].Type = 2;
-                                a.Start = a[accessing.Item1.X, accessing.Item1.Y];
-                                repeat = false;
-                                Console.WriteLine("Masuk");
-                                stack.Clear();
-                                stack.Push((a.Start, accessing.Item2));
-                            }
-                            else
-                            {
-                                solusi.Copy(temp);
-                                stack.Clear();
-                            }
-                        }
-                        else
-                        {
+                            count++;
+                            a[peta.Start.X, peta.Start.Y].Type = 3;
+                            a[accessing.Item1.X, accessing.Item1.Y].Type = 0;
+                            a[a.Start.X, a.Start.Y].Type = 2;
+                            a.Start = a[accessing.Item1.X, accessing.Item1.Y];
+                            repeat = false;
+                            Console.WriteLine("Masuk");
                             stack.Clear();
                             stack.Push((a.Start, accessing.Item2));
                         }
+                        else
+                        {
+                            solusi.Copy(temp);
+                            stack.Clear();
+                        }
                     }
-                    else //type ==2
+                    else
+                    {
+                        stack.Clear();
+                        stack.Push((a.Start, accessing.Item2));
+                    }
+                }
+                else //type ==2
+                {
+                    try
                     {
                         if (a[accessing.Item1.X, accessing.Item1.Y - 1].Type > 1 && !a[accessing.Item1.X, accessing.Item1.Y - 1].Accessed)
                         {
                             stack.Push((a[accessing.Item1.X, accessing.Item1.Y - 1], accessing.Item2));
                         }
+                    }
+                    catch (Exception e) { }
+                    try
+                    {
                         if (a[accessing.Item1.X, accessing.Item1.Y + 1].Type > 1 && !a[accessing.Item1.X, accessing.Item1.Y + 1].Accessed)
                         {
                             stack.Push((a[accessing.Item1.X, accessing.Item1.Y + 1], accessing.Item2));
                         }
+                    }
+                    catch (Exception e) { }
+                    try
+                    {
                         if (a[accessing.Item1.X - 1, accessing.Item1.Y].Type > 1 && !a[accessing.Item1.X - 1, accessing.Item1.Y].Accessed)
                         {
                             stack.Push((a[accessing.Item1.X - 1, accessing.Item1.Y], accessing.Item2));
                         }
+                    }
+                    catch (Exception e) { }
+                    try
+                    {
                         if (a[accessing.Item1.X + 1, accessing.Item1.Y].Type > 1 && !a[accessing.Item1.X + 1, accessing.Item1.Y].Accessed)
                         {
                             stack.Push((a[accessing.Item1.X + 1, accessing.Item1.Y], accessing.Item2));
                         }
                     }
+                    catch (Exception e) { }
                 }
-
                 a[accessing.Item1.X, accessing.Item1.Y].Accessed = true;
             }
         }
